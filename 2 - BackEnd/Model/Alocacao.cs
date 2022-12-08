090c2e22bd774185fca4620c2e32584a7217a3f8
Grinduim <viniciusgribelperson@gmail.com>
2022-12-08 10:58:55 -0300

    telas estrão com reguisição, baSTA FAZER  a logica das comunicações

--- 2 - BackEnd/Model/Alocacao.cs
+++ 2 - BackEnd/Model/Alocacao.cs
@@ -5,20 +5,20 @@ public class Alocacao
 {
     public int ID { get; set; }
     public int Area { get; set; }
-    public Automoveis Automovel {get;set;}
-    public Concessionarias Concessionaria {get; set;}
+    public Automoveis Automovel { get; set; }
+    public Concessionarias Concessionaria { get; set; }
     public int Quantidade { get; set; }
 
 
     public static IEnumerable<object> GetAllArea()
     {
-        using(var context = new Context())
+        using (var context = new Context())
         {
             var Areas = context.Alocacao.GroupBy(q => q.Area)
-                .Select(cl => new 
+                .Select(cl => new
                 {
                     Quantidade = cl.Sum(a => a.Quantidade),
-                    ID = cl.First().Area 
+                    ID = cl.First().Area
                 })
                 .ToList();
             return Areas;
@@ -27,7 +27,7 @@ public class Alocacao
 
     public static List<Automoveis> GetAllCarsAvaiable(int Area)
     {
-        using(var context = new Context())
+        using (var context = new Context())
         {
             var response = context.Alocacao.Where(q => q.Area == Area && q.Quantidade > 0).Select(q => q.Automovel).ToList();
 
@@ -37,23 +37,31 @@ public class Alocacao
 
     public static List<Concessionarias> GetAvailableConcessionariaWhereHaveSpecificCarAndArea(int AreaID, int CarID)
     {
-        using(var context = new Context())
+        using (var context = new Context())
         {
-            var response = context.Alocacao.Where(q => q.Automovel.ID == CarID && q.Area == AreaID && q.Quantidade > 0).Select(q=>q.Concessionaria).ToList();
+            var response = context.Alocacao.Where(q => q.Automovel.ID == CarID && q.Area == AreaID && q.Quantidade > 0).Select(q => q.Concessionaria).ToList();
             return response;
         }
     }
 
-    public static bool SellCar(int CarID, int AreaID,int ConcessionariaID)
+    public static bool SellCar(int CarID, int AreaID, int ConcessionariaID)
     {
 
         using (var context = new Context())
         {
-            var obj = context.Alocacao.Where(q => q.Automovel.ID == CarID && q.Area == AreaID && q.Concessionaria.ID == ConcessionariaID).Single();
+            try
+            {
+                var obj = context.Alocacao.Where(q => q.Automovel.ID == CarID && q.Area == AreaID && q.Concessionaria.ID == ConcessionariaID).Single();
 
-            obj.Quantidade = obj.Quantidade - 1;
-            context.SaveChanges();
-            return true;
+                obj.Quantidade = obj.Quantidade - 1;
+                context.SaveChanges();
+                return true;
+
+            }
+            catch{
+                return false;
+            }
+
 
         }
     }
